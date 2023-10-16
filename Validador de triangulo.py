ae4f4221b8bf8e115bcd4c77f718162a75902b75
HenriqueMiaciro <130669699+HenriqueMiaciro@users.noreply.github.com>
2023-10-16 15:14:09 -0300

    -- commit inicial miguel

---
+++ Validador de triangulo.py
@@ -0,0 +1,18 @@
+# validador do triangulo
+
+# apresentação
+
+print('            -- validador de triangulo   ')
+
+# Entrada
+
+a = int(input('LADO A'))
+b = int(input('LADO B'))
+c = int(input('LADO C'))
+
+#Processamento e saida
+if a < (b + c) and b < (a + c) and c < (a + b):
+    print('\n{}, {} e {} formam um triangulo'.format(a,b,c))
+else:
+    print('\n{}, {} e {}, não formam triangulo'.format(a,b,c))
+
